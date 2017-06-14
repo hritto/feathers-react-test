75ed531d86c8c19070721d9bf03db34bc43c81fa
apeficaz <hritto@gmail.com>
2017-06-14 15:03:33 +0200

    Form validation

--- client/src/components/commons/grid_manager/inputs/DropDown.jsx
+++ client/src/components/commons/grid_manager/inputs/DropDown.jsx
@@ -5,20 +5,30 @@ import R from 'ramda'
 const DropdownSelection = (props) => {
   let config = props.props.model.config;
   let el_config = R.find(R.propEq('name', props.campo))(config.fields);
-  let label = ''
   if(el_config.state === 'error'){
-    label = <Label basic color='red' pointing='below'>{el_config.message}</Label>
+    return (
+      <Form.Field>
+        <label>{el_config.label}</label>
+        <Label basic color='red' pointing='below'>{el_config.message}</Label>
+        <Dropdown error placeholder='Seleccionar...'
+              fluid selection
+              options={props.props.model.config.combo_values[props.campo]}
+              onChange={props.change}
+              name={props.campo}
+              value={props.state[props.campo]} />
+      </Form.Field>
+    );
   }
   return (
   <Form.Field>
     <label>{el_config.label}</label>
-    {label}
     <Dropdown placeholder='Seleccionar...'
           fluid selection
           options={props.props.model.config.combo_values[props.campo]}
           onChange={props.change}
           name={props.campo}
           value={props.state[props.campo]} />
-  </Form.Field>)}
+  </Form.Field>)
+}
 
 export default DropdownSelection
